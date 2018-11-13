52de1acadee13c4420f5e2116ede4d50983616a0
JunChengWU <15272762621@163.com>
2018-11-13 12:07:30 +0800

    Update Assets/Test.cs
    测试修改使用

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
+    string aaa = "123456789";
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
